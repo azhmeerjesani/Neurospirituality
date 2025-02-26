00f2280cf1e288977687624e16913e6110593967
MinerUNLEASHED <2020tray@gmail.com>
2025-02-25 20:36:55 -0600

    add home view to URL patterns

--- Neurospirituality/urls.py
+++ Neurospirituality/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from MainWeb import views
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
-    path('/', views.)
+    path('/', views.home, name='home')
 ]
